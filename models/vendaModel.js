6325b2f5e89884baa17e9ffa6c31c8de94197262
Guilherme Farias <109701085+GuiFxFarias@users.noreply.github.com>
2025-03-29 16:26:57 -0300

    feat: add PDF and update products page

--- models/vendaModel.js
+++ models/vendaModel.js
@@ -20,41 +20,39 @@ class VendaModel {
   //   }
 
   listar() {
-    const sql = `
-      SELECT
-    v.id,
-    v.idVenda,                           
-    c.nome AS nomeCliente,               
-    v.itemVenda,                         
-    v.tipoProduto,                       
-    v.dataProposta,
-    v.dataVenda,
-    v.status,  
-    CASE 
-      WHEN v.tipoProduto = '1' THEN pt.descricaoProduto
-      WHEN v.tipoProduto = '2' THEN pp.descricaoProduto
-      ELSE 'Produto não identificado'
-    END AS descricaoProduto,
-    CASE 
-      WHEN v.tipoProduto = '1' THEN pt.nSerieEquipamento
-      WHEN v.tipoProduto = '2' THEN pp.nSerieEquipamento
-      ELSE NULL
-    END AS nSerieEquipamento,
-    CASE 
-      WHEN v.tipoProduto = '1' THEN pt.nSerieSensor
-      ELSE NULL
-    END AS nSerieSensor,
-     CASE 
-      WHEN v.tipoProduto = '1' THEN pt.preco
-      WHEN v.tipoProduto = '2' THEN pp.preco
-      ELSE NULL
-    END AS preco
-    
-    FROM vendas v
-    JOIN clientes c ON v.idCliente = c.id
-    LEFT JOIN produto_transmissor pt ON v.tipoProduto = '1' AND v.idProduto = pt.id
-    LEFT JOIN produto_posicionador pp ON v.tipoProduto = '2' AND v.idProduto = pp.id
-    ORDER BY v.itemVenda;`
+    const sql = `SELECT 
+  v.id,
+  v.idVenda,
+  v.itemVenda,
+  v.tipoProduto,
+  v.dataProposta,
+  v.dataVenda,
+  v.status,
+  v.marca,
+  v.idProduto,
+  c.id AS idCliente,
+  c.nome AS nomeCliente,
+  c.cnpj,
+  c.nome_responsavel,
+  c.email,
+  c.telefone,
+  c.endereco,
+  c.cidade,
+  c.estado,
+  c.cep,
+  c.data_criacao,
+  CASE
+    WHEN v.tipoProduto = 'Transmissor' THEN pt.descricaoProduto
+    WHEN v.tipoProduto = 'Posicionador' THEN pp.descricaoProduto
+    ELSE 'Produto não identificado'
+  END AS descricaoProduto,
+  COALESCE(pt.nSerieEquipamento, pp.nSerieEquipamento) AS nSerieEquipamento,
+  COALESCE(pt.nSerieSensor, '') AS nSerieSensor,
+  COALESCE(pt.preco, pp.preco) AS preco
+FROM vendas v
+LEFT JOIN produto_transmissor pt ON pt.id = v.idProduto AND v.tipoProduto = 'Transmissor'
+LEFT JOIN produto_posicionador pp ON pp.id = v.idProduto AND v.tipoProduto = 'Posicionador'
+LEFT JOIN clientes c ON c.id = v.idCliente;`
     return this.executaQuery(sql, '')
   }
 
