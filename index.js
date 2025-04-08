dd839f19fc13c60390662772124ddabafe8fdee3
GuiFxFarias <guilhermefarias210303@gmail.com>
2025-04-08 20:05:44 -0300

    bugfix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const router = require("./routers/index.js");
-const cors = require("cors");
+const router = require('./routers/index.js');
+const cors = require('cors');
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: 'https://concert-psi.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   })
 );
@@ -14,9 +14,9 @@ router(app, express);
 
 app.listen(3001, (error) => {
   if (error) {
-    console.log("Error running server");
+    console.log('Error running server');
     return;
   }
 
-  console.log("Server is running on port 3001");
+  console.log('Server is running on port 3001');
 });
