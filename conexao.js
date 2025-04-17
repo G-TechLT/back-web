b21e8bfceb809d68ce805ccb031b330005073951
Guilherme Farias <109701085+GuiFxFarias@users.noreply.github.com>
2025-04-17 17:04:36 -0300

    feat: updateConexao

--- conexao.js
+++ conexao.js
@@ -1,19 +1,24 @@
 const mysql = require('mysql2');
 
-const conexao = mysql.createConnection({
+// Cria um pool de conexões
+const conexao = mysql.createPool({
   host: 'caboose.proxy.rlwy.net',
   port: 40989,
   user: 'root',
   password: 'VJbchauvFMCEmBRdfaWINdNAxfQQbJwL',
   database: 'railway',
-  connectTimeout: 10000,
+  waitForConnections: true, // Espera se todas as conexões estiverem ocupadas
+  connectionLimit: 10, // Número máximo de conexões abertas ao mesmo tempo
+  queueLimit: 0, // Sem limite de requisições na fila
 });
 
-conexao.connect((err) => {
+// Testa a conexão
+conexao.getConnection((err, connection) => {
   if (err) {
     console.error('Erro ao conectar no banco:', err.message);
   } else {
     console.log('Conectado ao banco de dados com sucesso!');
+    connection.release(); // Libera a conexão de volta para o pool
   }
 });
 
